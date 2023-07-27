dc31455e7459a2b639db20b96befdffea0d871bf
Charles Seaton <seatonct@gmail.com>
2023-07-27 14:48:46 -0500

    created functions in the PotteryCatalog.js module

---
+++ src/scripts/PotteryCatalog.js
@@ -0,0 +1,18 @@
+let inventory = [];
+
+export const toSellOrNotToSell = (potteryObj) => {
+  if (potteryObj.cracked === false) {
+    if (potteryObj.weight >= 6) {
+      potteryObj.price = 40;
+      inventory.push(potteryObj);
+    } else {
+      potteryObj.price = 20;
+      inventory.push(potteryObj);
+    }
+    return potteryObj;
+  }
+};
+
+export const usePottery = () => {
+  return Array.from(inventory);
+};
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,13 @@
 // Imports go first
 import { makePottery } from "./PotteryWheel.js";
 import { firePottery } from "./Kiln.js";
+import { toSellOrNotToSell, usePottery } from "./PotteryCatalog.js";
 // Make 5 pieces of pottery at the wheel
-let mug = makePottery("cylinder", 1, 4);
-let pot = makePottery("sphere", 5, 7);
-let chalice = makePottery("hourglass", 1, 5);
-let vase = makePottery("cylinder", 3, 8);
-let bowl = makePottery("round", 1, 3);
+let mug = makePottery("cylinder", 2, 4);
+let pot = makePottery("sphere", 10, 7);
+let chalice = makePottery("hourglass", 2, 5);
+let vase = makePottery("cylinder", 6, 8);
+let bowl = makePottery("round", 6, 3);
 
 firePottery(mug, 1000);
 firePottery(pot, 3000);
@@ -19,6 +20,15 @@ console.log(pot);
 console.log(chalice);
 console.log(vase);
 console.log(bowl);
+
+toSellOrNotToSell(mug);
+toSellOrNotToSell(pot);
+toSellOrNotToSell(chalice);
+toSellOrNotToSell(vase);
+toSellOrNotToSell(bowl);
+
+let catalog = usePottery();
+console.log(catalog);
 
 // Fire each piece of pottery in the kiln
 
